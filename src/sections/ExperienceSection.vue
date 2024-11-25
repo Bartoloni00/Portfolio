537e4440d4cc7499a09aebde4b0069f8b66ee865
bartoloni00 <bartoloniabraham@gmail.com>
2024-11-25 10:57:20 -0300

    feat: job experience section

--- src/sections/ExperienceSection.vue
+++ src/sections/ExperienceSection.vue
@@ -1,12 +1,38 @@
 <script setup>
-import JobCard from '../components/JobCard.vue';
 import jobExperience from '../utils/jobExperience';
+import PublishCard from '../components/PublishCard.vue';
+
+const imgs = ['tutoria.jpg', 'it-point.jpeg', 'sol-victoria-hotel-spa.jpg'];
+const emojis = [
+    String.fromCodePoint(0x1F9D1, 0x200D, 0x1F3EB),
+    String.fromCodePoint(0x1F4BB),
+    String.fromCodePoint(0x1F3B0)
+];
 </script>
 <template>
     <section class="mt-16">
         <h2 class="text-3xl text-text text-center" id="experience">Experiencia</h2>
-        <div class="flex flex-wrap justify-between gap-4 mx-auto">
-            <JobCard class="max-w-sm mx-4 my-4" v-for="job in jobExperience" :key="job.title" :job="job"/>
+        <div v-for="job, index in jobExperience" :key="job.title" :job="job">
+            <PublishCard :tag="`${emojis[index]} ${job.company}`" :content="{
+            title: job.title,
+            date: job.date,
+            place: job.location,
+            source: {
+                    img: '/public/'+ imgs[index],
+                    alt: job.title + ' en ' + job.company,
+                }
+        }">
+        <ul class="mt-2">
+            <li v-for="responsibility in job.responsibilities" :key="responsibility" class="mb-2 text-text flex items-start justify-start">
+                <i class="pi pi-check-circle" title="tiempo en la empresa"></i> <span class="ml-2 inline-block">{{ responsibility }}</span>
+            </li>
+        </ul>
+        <ul class="flex flex-wrap gap-2 justify-center mt-2">
+            <li v-for="skill in job.skills" :key="skill" class="mb-2 border border-primary/45 py-1 px-2 rounded-md bg-primary/10 text-text text-xs font-semibold">
+                {{ skill }}
+            </li>
+        </ul>
+        </PublishCard>
         </div>
     </section>
 </template>
